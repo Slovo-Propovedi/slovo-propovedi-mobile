12ed9084ab9f6415bf6fe275b4fda09f4b4254f8
Egor Slobodskoy <egoreast.true@gmail.com>
2023-09-02 20:49:33 +0300

    si-9# updated packages. Return SafeAreaView in the listen page

--- src/pages/listen/ui.tsx
+++ src/pages/listen/ui.tsx
@@ -1,22 +1,22 @@
 import React from 'react';
-import { ScrollView, StyleSheet } from 'react-native';
-import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { ScrollView, StatusBar, StyleSheet } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 import { ListenStackParamName, ListenStackScreenProps } from 'routing';
 import { SermonsOnBibleSlider, TopicalListSlider, NewSermonsSlider } from 'widgets';
 
 export const ListenScreen: React.FC<
   ListenStackScreenProps<ListenStackParamName.ListenHome>
-> = () => {
-  const { top } = useSafeAreaInsets();
+> = () => (
+  <SafeAreaView style={styles.container}>
+    <StatusBar backgroundColor='transparent' barStyle='dark-content' translucent />
 
-  return (
-    <ScrollView style={[styles.content, { top }]}>
+    <ScrollView style={styles.content}>
       <NewSermonsSlider />
       <SermonsOnBibleSlider />
       <TopicalListSlider />
     </ScrollView>
-  );
-};
+  </SafeAreaView>
+);
 
 const styles = StyleSheet.create({
   container: {
