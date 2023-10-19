7dd324aaa5f35e7c31c8fd4122c3a2822eb75fa8
Egor Slobodskoy <egoreast.true@gmail.com>
2023-10-19 23:49:47 +0300

    added script for all checking code rules

--- install.sh
+++ install.sh
@@ -4,9 +4,7 @@
 if [ ! -f .git/hooks/pre-commit ]; then
   echo "Creating pre-commit hook..."
   echo "#!/bin/bash" > .git/hooks/pre-commit
-  echo "yarn run check-types" >> .git/hooks/pre-commit
-  echo "yarn run prettier:write" >> .git/hooks/pre-commit
-  echo "yarn run lint:fix" >> .git/hooks/pre-commit
+  echo "yarn check-project-rules" >> .git/hooks/pre-commit
   chmod +x .git/hooks/pre-commit
   echo -e "Done creating pre-commit hook.\n\n"
 fi
