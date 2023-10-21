c344eccc78e71d5537925f0abd01a42641a2a214
Egor Slobodskoy <egoreast.true@gmail.com>
2023-10-21 09:46:10 +0300

    added updates feature

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,5 +1,6 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import * as Notifications from 'expo-notifications';
+import * as Updates from 'expo-updates';
 import React, { useEffect } from 'react';
 import { useSermonPlayerControlsStore } from 'features/sermon-player-controls';
 import { type AudioPlayerData, usePlayer, usePlayerStore } from 'entities/player';
@@ -37,42 +38,59 @@ const App = () => {
     }),
   );
 
+  const onFetchUpdateAsync = async () => {
+    try {
+      const update = await Updates.checkForUpdateAsync();
+
+      if (update.isAvailable) {
+        await Updates.fetchUpdateAsync();
+        await Updates.reloadAsync();
+      }
+    } catch (error) {
+      // You can also add an alert() to see the error message in case of an error when fetching updates.
+      alert(`Error fetching latest Expo update: ${error}`);
+    }
+  };
+
+  const initPlayerData = async () => {
+    const [
+      [, storedCurrentAudio],
+      [, storedCurrentPlaylist],
+      [, currentSoundPosition],
+      [, currentSoundDuration],
+    ] = await AsyncStorage.multiGet([
+      CURRENT_AUDIO,
+      CURRENT_PLAYLIST,
+      CURRENT_SOUND_POSITION,
+      CURRENT_SOUND_DURATION,
+    ]);
+
+    setCurrentSoundPosition(Number(currentSoundPosition));
+    setCurrentSoundDuration(Number(currentSoundDuration));
+
+    if (storedCurrentPlaylist) {
+      const currentPlaylist = parseJSONToObject<PlaylistData>(storedCurrentPlaylist);
+
+      currentPlaylist && setCurrentPlaylist(currentPlaylist);
+    }
+
+    if (storedCurrentAudio) {
+      const currentAudio = parseJSONToObject<AudioPlayerData>(storedCurrentAudio);
+      if (currentAudio) {
+        setCurrentAudio(currentAudio);
+
+        const currentSound = await recreateSound(
+          currentAudio.audioUrl,
+          Number(currentSoundPosition),
+        );
+        setCurrentSound(currentSound);
+      }
+    }
+  };
+
   useEffect(() => {
-    (async () => {
-      const [
-        [, storedCurrentAudio],
-        [, storedCurrentPlaylist],
-        [, currentSoundPosition],
-        [, currentSoundDuration],
-      ] = await AsyncStorage.multiGet([
-        CURRENT_AUDIO,
-        CURRENT_PLAYLIST,
-        CURRENT_SOUND_POSITION,
-        CURRENT_SOUND_DURATION,
-      ]);
-
-      setCurrentSoundPosition(Number(currentSoundPosition));
-      setCurrentSoundDuration(Number(currentSoundDuration));
-
-      if (storedCurrentPlaylist) {
-        const currentPlaylist = parseJSONToObject<PlaylistData>(storedCurrentPlaylist);
-
-        currentPlaylist && setCurrentPlaylist(currentPlaylist);
-      }
-
-      if (storedCurrentAudio) {
-        const currentAudio = parseJSONToObject<AudioPlayerData>(storedCurrentAudio);
-        if (currentAudio) {
-          setCurrentAudio(currentAudio);
-
-          const currentSound = await recreateSound(
-            currentAudio.audioUrl,
-            Number(currentSoundPosition),
-          );
-          setCurrentSound(currentSound);
-        }
-      }
-    })();
+    onFetchUpdateAsync();
+    initPlayerData();
 
     return () => {
       unload();
