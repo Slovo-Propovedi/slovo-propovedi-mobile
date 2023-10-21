47a1020ecd40bcd1a9faea492d8db35122824344
Egor Slobodskoy <egoreast.true@gmail.com>
2023-10-21 14:12:42 +0300

    fixed testing with async-storage

--- jest.config.ts
+++ jest.config.ts
@@ -10,6 +10,7 @@ const config: Config = {
     '!**/jest.setup.js',
   ],
   preset: 'jest-expo',
+  setupFiles: ['./__mocks__/@react-native-async-storage/async-storage.js'],
   transformIgnorePatterns: [
     'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg|ky)',
   ],
