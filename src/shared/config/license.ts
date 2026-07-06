3b0401ff6e71b107e2af965f35a901a3f2fb5261
egoreast <egor.east@yandex.ru>
2026-07-06 19:05:19 +0300

    fix: license url

--- src/shared/config/license.ts
+++ src/shared/config/license.ts
@@ -1,6 +1,6 @@
 export const LICENSE_NAME = 'GPL-3.0'
 export const LICENSE_URL =
-  'https://git.lightnode.ru/Slovo_Propovedi/slovo-propovedi-mobile/-/blob/main/LICENSE'
+  'https://git.lightnode.ru/Slovo_Propovedi/slovo-propovedi-mobile/src/branch/main/LICENSE'
 export const PROJECT_URL = 'https://git.lightnode.ru/Slovo_Propovedi/slovo-propovedi-mobile/'
 export const COPYRIGHT_YEAR = '2026'
 export const COPYRIGHT_HOLDER = 'Slovo.Propovedi'
